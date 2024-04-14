cbf0bc549ade26fcbe182e43d3f5049bb14b6846
Pawar-Suraj <surajpawars3010@gmail.com>
2024-04-14 15:36:45 +0530

    order id message added

--- js/track-order.js
+++ js/track-order.js
@@ -10,13 +10,18 @@ function changeText(btnnum) {
     
 }
 
-function checkSubmitt() {
+
+function message(){
     const element = document.getElementById('myInput');
-    if (element.value == 101) {
-        
-        alert(element.value+ ' is  correct procees to next page' );
-    }
-    else {
-        alert(element.value+ ' is not correct' );
-    }
+    const messageElement = document.getElementById("orderID-message");
+
+  messageElement.textContent = ""; // Clear any previous message
+
+  if (element.value == 101) {
+    messageElement.textContent = "Order ID is valid!";
+    messageElement.style.color = "green";
+  } else {
+    messageElement.textContent = "Order ID is invalid.";
+    messageElement.style.color = "red";
+  }
 }
